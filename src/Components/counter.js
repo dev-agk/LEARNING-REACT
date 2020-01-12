82953fd9ab4f73265e33616f02baf8c78ea8a18d
dev-agk <abhilash.gopalakrishna@gmail.com>
2020-01-12 20:54:41 +0530

    substituting loops in react using maps

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -3,7 +3,8 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     //has any data the component needs
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   styles = {
@@ -19,12 +20,23 @@ class Counter extends Component {
         <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> 
           {this.formatCount()} 
         */}
-        <span style={this.styles} className="badge badge-primary m-2">
+        <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
