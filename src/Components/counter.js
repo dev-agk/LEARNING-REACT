14759c6e2cef6c00141fb2722d6342fa510bcc58
dev-agk <abhilash.gopalakrishna@gmail.com>
2020-01-12 20:40:04 +0530

    react components and state values

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -1,8 +1,36 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    //has any data the component needs
+    count: 0
+  };
+
+  styles = {
+    //TOLEARN: setting properties for styles? how does browser understnad this? as json?
+    fontWeight: "bold",
+    fontSIze: 20
+  };
   render() {
-    return <h1> First React App</h1>;
+    return (
+      <div>
+        {/* 
+        TOLEARN: anonymous object[{{}}]?
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2"> 
+          {this.formatCount()} 
+        */}
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
+
+  formatCount() {
+    //TOLEARN: OBJECT DESTRUCTURING
+    let { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import Counter from "./Components/counter";
 import * as serviceWorker from "./serviceWorker";
+//TOLEARN:WEBPACK CSS LOADER
 import "bootstrap/dist/css/bootstrap.css";
-
 ReactDOM.render(<Counter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
