a0adcfd3d202652313a94038c1b8e0cfc7751ca8
dev-agk <abhilash.gopalakrishna@gmail.com>
2020-01-13 14:01:12 +0530

    A bunch of questions-\(--)/-???

--- src/Components/counter.js
+++ src/Components/counter.js
@@ -7,11 +7,34 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  constructor() {
+    //need to call the constructor of Component class from where we are inheriting
+    //super(); -> what is this
+    //In JS every function is an object, therefore the bind property can be used on each function.
+    //this.handleIncrements.bind(this); -> why bind is needed?
+  }
+  //you can use arraow functions to inherit without constructors and bind()
+  // handleIncements= ->(){}
+  handleIncrements() {
+    //Questions?
+    //https://www.phpied.com/3-ways-to-define-a-javascript-class/ -> everything in javascript os actually an object.
+    //handleIncrements() -> Is a method of the class counter -> which is an object? If so shouldnt this return a reference to the class?
+    //Why is a constructor needed?
+    //Notes:
+    // In javascript if a function who is a member of the object -> this returns reference to that object.
+    // However if a standalone function is called it returns undefined in strict mode, and reference to the window object by default
+    // We use the bind() method to solve the above.
+    //does not work without the constructor
+    //https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
+    //this.state.count++; -> dont use this use setState();
+  }
+
   styles = {
     //TOLEARN: setting properties for styles? how does browser understnad this? as json?
     fontWeight: "bold",
     fontSIze: 20
   };
+
   render() {
     return (
       <div>
